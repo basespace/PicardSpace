23a3c6e083bc127801baf565c545ec6d70217929
Eric Smith <esmith@illumina.com>
2013-06-28 13:50:26 -0700

    Bug fix in inconsistent calculation of file size - now using 1000 bytes in a kilobyte

--- modules/picardSpace.py
+++ modules/picardSpace.py
@@ -625,9 +625,9 @@ def readable_bytes(size,precision=2):
     """
     suffixes=['B','KB','MB','GB','TB']
     suffixIndex = 0
-    while size > 1024:
+    while size > 1000:
         suffixIndex += 1 #increment the index of the suffix
-        size = size/1024.0 #apply the division
+        size = size/1000.0 #apply the division
     return "%.*f %s"%(precision,size,suffixes[suffixIndex])
     
 
